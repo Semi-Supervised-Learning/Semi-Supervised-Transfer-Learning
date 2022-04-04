32c8e62cebb8057f52c8cb6b9394b46fbdedd361
louris <983832469@qq.com>
2022-04-04 10:27:25 +0800

    update annotation for trainer.train.py

--- code/ssl_lib/trainer/train.py
+++ code/ssl_lib/trainer/train.py
@@ -15,6 +15,7 @@ from .supervised import supervised_train
 LABELED_FEAT_TABLES=None
 UNLABELED_FEAT_TABLES=None
 
+# 论文对应的那个H()公式计算掩码
 def get_mask(logits,threshold, num_class=10):
     ent = -(logits.softmax(1) * logits.log_softmax(1)).sum(1)
     threshold = threshold * math.log(num_class)
@@ -23,11 +24,13 @@ def get_mask(logits,threshold, num_class=10):
 
 def update_feat_table(cur_feat_l,cur_feat_u,feat_table_size_l=-1,feat_table_size_u=-1,mask_l=None, mask_u=None):
     global LABELED_FEAT_TABLES,UNLABELED_FEAT_TABLES
+    # 根据mask也就是ACK正则项选择置信度高的样本常驻内存
     if mask_l is not None:
         mask_l = mask_l.nonzero().flatten()
         mask_u = mask_u.nonzero().flatten()
         cur_feat_l=cur_feat_l[mask_l]
         cur_feat_u=cur_feat_u[mask_u]
+    # 更新常驻内存的table
     if feat_table_size_l>0:
         if LABELED_FEAT_TABLES is None:
             LABELED_FEAT_TABLES = cur_feat_l
@@ -39,6 +42,7 @@ def update_feat_table(cur_feat_l,cur_feat_u,feat_table_size_l=-1,feat_table_size
                 LABELED_FEAT_TABLES = LABELED_FEAT_TABLES[-feat_table_size_l:]
             if len(UNLABELED_FEAT_TABLES) > feat_table_size_u:
                 UNLABELED_FEAT_TABLES = UNLABELED_FEAT_TABLES[-feat_table_size_u:]
+        # 这里得注意，detach()将特征从计算图上分离出来，但是最近的cur_feat并没有从计算图上分离哦！
         feat_l = LABELED_FEAT_TABLES
         feat_u = UNLABELED_FEAT_TABLES
         LABELED_FEAT_TABLES=LABELED_FEAT_TABLES.detach()
@@ -49,6 +53,8 @@ def update_feat_table(cur_feat_l,cur_feat_u,feat_table_size_l=-1,feat_table_size
     
     return feat_l, feat_u
 
+# 以(16, 3)为例
+# 输出[0, 5, 10, 16]
 def interleave_offsets(batch, nu):
     groups = [batch // (nu + 1)] * (nu + 1)
     for x in range(batch - sum(groups)):
@@ -59,11 +65,29 @@ def interleave_offsets(batch, nu):
     assert offsets[-1] == batch
     return offsets
 
-
+# 以[(16, 3, 32, 32)] * 3 为例
+# 输出形状不变，只是交错位置
 def interleave(xy, batch):
     nu = len(xy) - 1
     offsets = interleave_offsets(batch, nu)
-    xy = [[v[offsets[p]:offsets[p + 1]] for p in range(nu + 1)] for v in xy]
+
+    # xy 的输出
+    # [[①，②，③],
+    #  [④, ⑤, ⑥],
+    #  [⑦, ⑧, ⑨]]
+    # [[(5, 3, 32, 32), (5, 3, 32, 32), (6, 3, 32, 32)],
+    #  [(5, 3, 32, 32), (5, 3, 32, 32), (6, 3, 32, 32)],
+    #  [(5, 3, 32, 32), (5, 3, 32, 32), (6, 3, 32, 32)]]
+    xy = [[v[offsets[p]:offsets[p + 1]] for p in range(nu + 1)] for v in xy]  
+
+    # xy原先是
+    # [[①，②，③],
+    #  [④, ⑤, ⑥],
+    #  [⑦, ⑧, ⑨]]
+    # 以下操作后变成
+    # [[①，⑤，⑨],
+    #  [④, ②, ⑥],
+    #  [⑦, ⑧, ③]]
     for i in range(1, nu + 1):
         xy[0][i], xy[i][i] = xy[i][i], xy[0][i]
     return [torch.cat(v, dim=0) for v in xy]
@@ -79,10 +103,12 @@ def infer_interleave(forward_fn, inputs_train,cfg,bs):
     else:
         inputs_interleave = [inputs_train]
 
+    # 存储中间层输出
     out_lists_inter = [forward_fn(inputs_interleave[0],return_fmap=True)]
     for inputs in inputs_interleave[1:]:
         out_lists_inter.append(forward_fn(inputs,return_fmap=True))
 
+    # 保存最后一层全连接层的输出和卷积层最后一层的输出
     for ret_id in [-1,-3]:
         ret_list=[]
         for o_list in out_lists_inter:
@@ -105,6 +131,8 @@ def infer_interleave(forward_fn, inputs_train,cfg,bs):
             cur_feat_u = feat_u_w
             cur_feat_s = feat_u_s
             feat_target = torch.cat((feat_l, feat_u_w), dim=0)
+    
+    # 返回最后一层和卷积层最后一层的输出
     return  logits_l,logits_u_w, logits_u_s,cur_feat_l,cur_feat_u,cur_feat_s,feat_target
 
 def train(epoch,train_loader , model,source_model,ema_teacher,optimizer,lr_scheduler, ssl_alg, cfg,device):
@@ -136,6 +164,7 @@ def train(epoch,train_loader , model,source_model,ema_teacher,optimizer,lr_sched
     for batch_idx, (data_l, data_u) in enumerate(train_loader):
         inputs_l, labels = data_l
         inputs_l, labels = inputs_l.to(device), labels.to(device)
+        # inputs_u_w, inputs_u_s为无标签数据的弱增强和强增强表示
         inputs_u_w, inputs_u_s, labels_u = data_u
         inputs_u_w, inputs_u_s, labels_u = inputs_u_w.to(device), inputs_u_s.to(device), labels_u.to(device)
         inputs_train = torch.cat((inputs_l, inputs_u_w, inputs_u_s), dim=0)
@@ -153,11 +182,15 @@ def train(epoch,train_loader , model,source_model,ema_teacher,optimizer,lr_sched
         feat_u_w, feat_u_s = ret_list[-3][bs:].chunk(2)
         feat_target = torch.cat((feat_l, feat_u_w), dim=0)
         '''
+        # 输出的是
+        # 全连接层的输出 和 卷积层最后一层的输出
         logits_l,logits_u_w, logits_u_s,feat_l,feat_u_w,feat_u_s,feat_target  = infer_interleave(forward_fn, inputs_train,cfg, bs)
         L_supervised = F.cross_entropy(logits_l, labels)
 
         # calc total loss
         coef = scheduler.linear_warmup(cfg.coef, cfg.warmup_iter, cur_iteration+1)
+        
+        # 有标签数据的交叉熵损失
         L_consistency = torch.zeros_like(L_supervised) 
         mask = torch.zeros_like(L_supervised)   
         if cfg.coef > 0:
@@ -181,25 +214,31 @@ def train(epoch,train_loader , model,source_model,ema_teacher,optimizer,lr_sched
                 tea_forward = ema_forward_fn
             )
             model.module.update_batch_stats(True)
+            # 无标签数据的一致性损失
+            # 损失函数包含交叉熵、均方误差，targets为SSL方法生成的伪标签
             L_consistency = consistency_criterion(y, targets, mask)
 
-        
+        # 计算ARC正则项L_mmd
         L_mmd = torch.zeros_like(L_supervised) 
         mmd_mask_u = torch.zeros_like(L_supervised)   
         if cfg.lambda_mmd>0:
+            # 论文对应的ARC正则项，分为有标签和无标签两种
             mmd_mask_l = get_mask(logits_l,cfg.mmd_threshold,  num_class=cfg.num_classes)
             mmd_mask_u = get_mask(logits_u_w,cfg.mmd_threshold,  num_class=cfg.num_classes)
             if mmd_mask_l.sum()>0 and mmd_mask_u.sum()>0:
+                # 更新table，table采用常驻内存的方式保存最近批次的置信度高的特征（置信度通过ARC正则项表示并选择常驻的特征）
                 cur_feat_l, cur_feat_u = update_feat_table(feat_l,feat_u_w,cfg.mmd_feat_table_l,cfg.mmd_feat_table_u, mask_l=mmd_mask_l, mask_u=mmd_mask_u)
                 if cur_iteration>cfg.reg_warmup and len(cur_feat_l)>20:
                     L_mmd = mmd_criterion(cur_feat_l, cur_feat_u)
 
+        # 计算AKC正则项L_kd
         L_kd = torch.zeros_like(L_supervised)
         kd_mask = torch.zeros_like(L_supervised)   
         if cfg.lambda_kd>0:
             src_inputs = torch.cat((inputs_l, inputs_u_w), dim=0)
            
             with torch.no_grad():
+                # source_modle为预训练在ImageNet上的模型
                 src_out_list = source_model(src_inputs,return_fmap=True)
             src_logits = src_out_list[-1]
             kd_mask = get_mask(src_logits,cfg.kd_threshold, num_class=1000)
@@ -208,8 +247,9 @@ def train(epoch,train_loader , model,source_model,ema_teacher,optimizer,lr_sched
             
             del src_out_list,  src_inputs
     
-        lambda_mmd = scheduler.linear_warmup(cfg.lambda_mmd, cfg.reg_warmup_iter, cur_iteration+1)
-        lambda_kd = scheduler.linear_warmup(cfg.lambda_kd, cfg.reg_warmup_iter, cur_iteration+1)
+        lambda_mmd = scheduler.linear_warmup(cfg.lambda_mmd, cfg.reg_warmup_iter, cur_iteration + 1)
+        lambda_kd = scheduler.linear_warmup(cfg.lambda_kd, cfg.reg_warmup_iter, cur_iteration + 1)
+        # 计算最终的loss
         loss = L_supervised + coef * L_consistency + lambda_mmd * L_mmd + lambda_kd * L_kd
 
         # update parameters
@@ -220,6 +260,7 @@ def train(epoch,train_loader , model,source_model,ema_teacher,optimizer,lr_sched
         optimizer.step()
         lr_scheduler.step()
 
+        # mixup方式更新ema_teacher
         if cfg.ema_teacher:
             model_utils.ema_update(
                 ema_teacher, model, cfg.ema_teacher_factor,
